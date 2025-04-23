4fc0e5d8b2c2d36bba686c3c396350e53ebc858d
Tohirjon-dev <tohirjonmuqimov79@gmail.com>
2025-04-23 15:44:15 +0500

    yangilanishlar qo'shilid

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,6 +11,8 @@ import {
   Res,
   UseGuards,
   Req,
+  UseInterceptors,
+  UploadedFile,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { RegisterDto } from './dto/register-user.dto';
@@ -19,24 +21,42 @@ import { Request, Response } from 'express';
 import { AuthGuard } from 'src/common/guards/auth.guard';
 import { RolesGuard } from 'src/common/guards/roles.guard';
 import { Roles } from 'src/common/decorators/role.decorator';
+import { diskStorage, Multer } from 'multer';
+import { extname } from 'path';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+const storage = diskStorage({
+  destination: './userImages',
+  filename: (req, file, cb) => {
+    const uniquName = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, uniquName + extname(file.originalname));
+  },
+});
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
   @Post('register')
-  async register(@Body() body: RegisterDto) {
+  @UseInterceptors(FileInterceptor('image', { storage }))
+  async register(
+    @Body() body: RegisterDto,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
     try {
-      return await this.usersService.register(body);
+      const imageUrl = file.path;
+      return await this.usersService.register(body, imageUrl);
     } catch (error) {
       throw new HttpException(error.message, HttpStatus.ALREADY_REPORTED);
     }
   }
   @Post('login')
+  @UseInterceptors(FileInterceptor('image', { storage }))
   async login(
     @Body() body: LoginDto,
+    @UploadedFile() file: Express.Multer.File,
     @Res({ passthrough: true }) res: Response,
   ) {
-    const data = await this.usersService.login(body);
+    const data = await this.usersService.login(body, file.path);
     const token = data.token;
     try {
       res.cookie('authToken', token, { httpOnly: true });
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -10,6 +10,13 @@ import { RegisterDto } from './dto/register-user.dto';
 import { LoginDto } from './dto/login-user.dto';
 import { JwtService } from '@nestjs/jwt';
 import { userPayload } from 'src/common/types/user-payload.types';
+import * as fs from 'fs';
+import * as crypto from 'crypto';
+
+function getFileHash(path: string): string {
+  const fileBuffer = fs.readFileSync(path);
+  return crypto.createHash('sha256').update(fileBuffer).digest('hex');
+}
 
 @Injectable()
 export class UsersService {
@@ -17,15 +24,21 @@ export class UsersService {
     private prisma: PrismaService,
     private jwt: JwtService,
   ) {}
-  async register(registerDto: RegisterDto) {
+  async register(registerDto: RegisterDto, imageUrl: string) {
+    if (imageUrl.length === 0)
+      throw new ForbiddenException('Rasm yuklashingiz shart');
     const findUSer = await this.prisma.user.findUnique({
       where: { email: registerDto.email },
     });
     if (findUSer)
       throw new ConflictException("Siz allaqachion ro'yxatdan o'tgansiz");
-    return await this.prisma.user.create({ data: registerDto });
+    return await this.prisma.user.create({
+      data: { ...registerDto, imageUrl },
+    });
   }
-  async login(loginDto: LoginDto) {
+  async login(loginDto: LoginDto, UploadImagePath: string) {
+    if (UploadImagePath.length === 0)
+      throw new ForbiddenException('Rasmni yuklang!!!');
     const findUSer = await this.prisma.user.findUnique({
       where: { email: loginDto.email },
     });
@@ -35,6 +48,11 @@ export class UsersService {
       );
     if (findUSer.password !== loginDto.password)
       throw new ForbiddenException("Parol noto'g'ri");
+    const hash1 = getFileHash(findUSer.imageUrl);
+    const hash2 = getFileHash(UploadImagePath);
+
+    fs.unlinkSync(UploadImagePath);
+    if (hash1 !== hash2) throw new ForbiddenException("Rasm noto'g'ri");
     const token = this.jwt.sign({
       id: findUSer.id,
       email: findUSer.email,
@@ -42,7 +60,7 @@ export class UsersService {
     });
     return { token, message: 'Tizimga xush kelibsiz' };
   }
-  
+
   async getme(data: userPayload) {
     return this.prisma.user.findUnique({ where: { id: data.id } });
   }
